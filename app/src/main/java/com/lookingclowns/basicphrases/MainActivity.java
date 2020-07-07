91a369167cf561097e7a45af2ee1efaa45b8416a
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-07 21:23:02 +0200

    Refactor of the function. Now each sound is directly taken, instead of searching in an array

--- app/src/main/java/com/lookingclowns/basicphrases/MainActivity.java
+++ app/src/main/java/com/lookingclowns/basicphrases/MainActivity.java
@@ -7,17 +7,6 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    int[] SOUND_IDS = {
-            R.raw.doyouspeakenglish,
-            R.raw.goodevening,
-            R.raw.hello,
-            R.raw.howareyou,
-            R.raw.ilivein,
-            R.raw.mynameis,
-            R.raw.please,
-            R.raw.welcome
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +14,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void playSound(View view) {
-        int sound = Integer.parseInt(view.getTag().toString());
-        play(sound);
+        play(view.getTag().toString());
     }
 
-    public void play(int sound) {
-        MediaPlayer.create(this, SOUND_IDS[sound]).start();
+    public void play(String soundName) {
+        MediaPlayer.create(
+                this,
+                getResources()
+                        .getIdentifier(
+                                soundName,
+                                "raw",
+                                getPackageName()
+                        )
+        ).start();
     }
 }
