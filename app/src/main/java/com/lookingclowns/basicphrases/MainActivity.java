511402bf23ea2f4d9b1dcbb0e0a366b2a9226cd0
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-07 21:08:10 +0200

    Now when a button is clicked, a sound is played

--- app/src/main/java/com/lookingclowns/basicphrases/MainActivity.java
+++ app/src/main/java/com/lookingclowns/basicphrases/MainActivity.java
@@ -1,6 +1,8 @@
 package com.lookingclowns.basicphrases;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playSound(View view) {
+        int sound = Integer.parseInt(view.getTag().toString());
+        play(sound);
+    }
+
+    public void play(int sound) {
+        MediaPlayer.create(this, SOUND_IDS[sound]).start();
     }
 }
