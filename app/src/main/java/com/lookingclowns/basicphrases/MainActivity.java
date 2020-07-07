241ad1d947a045170e04157e338bfca4b972d412
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-07 21:02:33 +0200

    Added sound ids to MainActivity

--- app/src/main/java/com/lookingclowns/basicphrases/MainActivity.java
+++ app/src/main/java/com/lookingclowns/basicphrases/MainActivity.java
@@ -5,6 +5,16 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    int[] SOUND_IDS = {
+            R.raw.doyouspeakenglish,
+            R.raw.goodevening,
+            R.raw.hello,
+            R.raw.howareyou,
+            R.raw.ilivein,
+            R.raw.mynameis,
+            R.raw.please,
+            R.raw.welcome
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
